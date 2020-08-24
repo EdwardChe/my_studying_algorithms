6cd023c3e8ec13f9aaea7976fd556062581503f8
Edward <eduard.chernyadiev@gmail.com>
2020-08-24 19:53:47 +0300

    update find_small_element

--- algorithms.py
+++ algorithms.py
@@ -34,7 +34,9 @@ def find_small_element(mass):
     small_element_index = 0
     for i in range(1, len(mass)):
         if mass[i] < small_element:
-
+        small_element = mass[i]
+        small_element_index = i
+    return small_element_index
 
 
 
